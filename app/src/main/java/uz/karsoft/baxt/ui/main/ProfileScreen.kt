f1d6a3645f8fadcad0eab12722a84b99fdf7f1ed
Umida Reimbaeva <umreimbaeva@gmail.com>
2024-09-20 22:32:54 +0500

    prof ui

--- app/src/main/java/uz/karsoft/baxt/ui/main/ProfileScreen.kt
+++ app/src/main/java/uz/karsoft/baxt/ui/main/ProfileScreen.kt
@@ -3,5 +3,5 @@ package uz.karsoft.baxt.ui.main
 import androidx.fragment.app.Fragment
 import uz.karsoft.baxt.R
 
-class ProfileScreen: Fragment(R.layout.layout_home) {
+class ProfileScreen: Fragment(R.layout.layout_profile) {
 }
